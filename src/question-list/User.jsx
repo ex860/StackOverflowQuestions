a15a338a1e180b8343d1153cc6b5cd03937301aa
Yu-Hsien Yeh <ex8600@gmail.com>
2023-04-09 22:36:49 +0800

    feat: Implement lazy loading for user images

--- src/question-list/User.jsx
+++ src/question-list/User.jsx
@@ -1,12 +1,28 @@
-import react from 'react';
+import react, { useEffect, useRef } from 'react';
 import './User.scss';
-
 
 const User = (props) => {
     const { display_name, profile_image } = props;
+    const imgRef = useRef(null);
+
+    useEffect(() => {
+        const callback = (entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting && entry?.target) {
+                    entry.target.src = profile_image;
+                    imageObserver.unobserve(entry.target);
+                }
+            });
+        };
+        const imageObserver = new IntersectionObserver(callback, {});
+        imageObserver.observe(imgRef.current);
+        return () => {
+            imageObserver.disconnect();
+        };
+    }, []);
     return (
         <div className="user">
-            <img className="user__image" src={profile_image} />
+            <img className="user__image" ref={imgRef} />
             {display_name}
         </div>
     );
